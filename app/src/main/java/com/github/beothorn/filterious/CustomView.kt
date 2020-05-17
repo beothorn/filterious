199d4669d14cb8d836c77210820b48d027423071
Carol <carolinecollaco@gmail.com>
2020-05-17 23:57:35 +0200

    Experiment: set blue to 0

--- app/src/main/java/com/github/beothorn/filterious/CustomView.kt
+++ app/src/main/java/com/github/beothorn/filterious/CustomView.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.View
 import android.widget.ImageView
 import androidx.core.graphics.applyCanvas
+import androidx.core.graphics.get
 import androidx.core.graphics.withMatrix
 
 class CustomView(context: Context, attrs: AttributeSet) : ImageView(context, attrs) {
@@ -16,6 +18,17 @@ class CustomView(context: Context, attrs: AttributeSet) : ImageView(context, att
     private var bitmap:Bitmap                   
     init{
         bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.sampleimage)
+        for (i in 0..bitmap.width){
+            for (j in 0..bitmap.height){
+                val pixel = bitmap[i,j]
+                val red = Color.red(pixel)
+                val green = Color.green(pixel)
+                val blue = 0//Color.blue(pixel)
+                val alpha = Color.alpha(pixel)
+                bitmap.setPixel(i, j, Color.argb(alpha, red, green, blue))
+            }
+        }
+
         setImageBitmap(bitmap)
     }
 
